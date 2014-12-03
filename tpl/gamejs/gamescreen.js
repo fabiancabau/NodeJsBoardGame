138a6ce9828a3835278fe1a9bda3a748fe8a59c8
Fabian Venturini Cabau <W3S\fcabau@ub-aqa-175.w3s.local>
2014-12-03 13:57:49 -0200

    Added Phaser

--- tpl/gamejs/gamescreen.js
+++ tpl/gamejs/gamescreen.js
@@ -1,204 +1,80 @@
-$(document).ready(function(){
-	var socket = io.connect();
-	var heroes = Array();
-	
+	var game = new Phaser.Game(1280, 720, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
+	function preload() {
 
-	// Create the canvas
-	var canvas = document.createElement("canvas");
-	var ctx = canvas.getContext("2d");
-	canvas.width = 512;
-	canvas.height = 480;
+		//  You can fill the preloader with as many assets as your game requires
 
+		//  Here we are loading an image. The first parameter is the unique
+		//  string by which we'll identify the image later in our code.
 
-	var gridBlockWidth = 40;
-	var gridBlockHeight = 40;
+		//  The second parameter is the URL of the image (relative)
+		game.load.image('phaser', 'hero.png');
+		game.load.image('background', 'map.jpg');
 
-	document.body.appendChild(canvas);
-
-
-
-	function Cell(row, column, clickx, clicky) {
-	    this.row = row;
-	    this.column = column;
-	    this.clickx = clickx;
-	    this.clicky = clicky;
-	}
-
-	function getCursorPosition(e) {
-	    /* returns Cell with .row and .column properties */
-	    var x;
-	    var y;
-	    if (e.pageX != undefined && e.pageY != undefined) {
-			x = e.pageX;
-			y = e.pageY;
-	    }
-	    else {
-			x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-			y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-	    }
-
-	    x -= canvas.offsetLeft;
-	    y -= canvas.offsetTop;
-	    x = Math.min(x, canvas.width * gridBlockWidth);
-	    y = Math.min(y, canvas.height * gridBlockHeight);
-	    var cell = new Cell(Math.floor(y/gridBlockHeight), Math.floor(x/gridBlockWidth), x, y);
-	    console.log(cell);
-	    socket.emit('sendClick', cell);
-	    //return cell;
 	}
 
 
 
+	var sprite;
+	var background;
+	var gotoX, gotoY = 0;
 
 
-	//grid width and height
-	var bw = 440;
-	var bh = 400;
-	//padding around grid
-	var p = 35;
-	//size of canvas
-	var cw = bw + (p*2) + 1;
-	var ch = bh + (p*2) + 1;
+	function create() {
+		gotoX = game.world.centerX;
+		gotoY = game.world.centerY;
 
-	function drawBoard(){
-	    for (var x = 0; x <= bw; x += gridBlockWidth) {
-	        ctx.moveTo(0.5 + x + p, p);
-	        ctx.lineTo(0.5 + x + p, bh + p);
-	    }
+		//  To make the sprite move we need to enable Arcade Physics
+		game.physics.startSystem(Phaser.Physics.ARCADE);
 
+		//	Create a background image
+		background = game.add.sprite(0, 0, 'background');
 
-	    for (var x = 0; x <= bh; x += gridBlockHeight) {
-	        ctx.moveTo(p, 0.5 + x + p);
-	        ctx.lineTo(bw + p, 0.5 + x + p);
-	    }
+		//	Enable input to it so we can grab click events
+		background.inputEnabled = true;
 
-	    ctx.strokeStyle = "black";
-	    ctx.stroke();
+		//	pixelPerfectClick is expensive but totally needed
+		game.input.pixelPerfectClick = true;
+
+		//	Hand Cursor is nicer than normal, no? :)
+		background.input.useHandCursor = true;
+
+		//	Add the hero sprite to the world (we're gonna use a for loop to add N heroes)
+		sprite = game.add.sprite(game.world.centerX, game.world.centerY, 'phaser');
+		sprite.anchor.set(0.5);
+
+		//  And enable the Sprite to have a physics body:
+		game.physics.arcade.enable(sprite);
+
 	}
 
-	// Background image
-	var bgReady = false;
-	var bgImage = new Image();
-	bgImage.onload = function () {
-		bgReady = true;
-	};
-	bgImage.src = "background.png";
+	function update () {
 
-	function createHero(hero) {
-		// Hero image
-		hero.ready = false;
-		hero.image = new Image();
-		hero.image.onload = function () {
-			hero.ready = true;
-		};
-		hero.image.src = "hero.png";
-		// Game objects
-		hero.speed = 256 // movement in pixels per second
-
-		return hero;
-	}	
-
-	// Handle keyboard controls
-	var keysDown = {};
-
-	addEventListener("keydown", function (e) {
-		keysDown[e.keyCode] = true;
-	}, false);
-
-	addEventListener("keyup", function (e) {
-		delete keysDown[e.keyCode];
-	}, false);
-
-	// Update game objects
-	var update = function (modifier) {
-		if (38 in keysDown) { // Player holding up
-			socket.emit('sendInput', {'key': 'up', 'modifier': modifier});
+		//On click, set the target X and Y
+		if (game.input.mousePointer.isDown) {
+			gotoX = game.input.mousePointer.x;
+			gotoY = game.input.mousePointer.y;
 		}
-		if (40 in keysDown) { // Player holding down
-			socket.emit('sendInput', {'key': 'down', 'modifier': modifier});
-		}
-		if (37 in keysDown) { // Player holding left
-			socket.emit('sendInput', {'key': 'left', 'modifier': modifier});
-		}
-		if (39 in keysDown) { // Player holding right
-			socket.emit('sendInput', {'key': 'right', 'modifier': modifier});
-		}
-	};
 
-	// Reset the game when the player catches a monster
-	var reset = function () {
-		for (var x = 0; x < heroes.length; x++) {
-			heroes[x].x = canvas.width / 2;
-			heroes[x].y = canvas.height / 2;
-		}
-		
-	};
+		if (game.physics.arcade.distanceToXY(sprite, gotoX, gotoY) > 8) {
 
-	// Draw everything
-	var render = function (heroes) {
-		//drawBoard();
-
-		for (var i = 0; i < heroes.length; i++) { 
-			if (heroes[i].ready) {
-				ctx.drawImage(heroes[i].image, heroes[i].x, heroes[i].y);
+			if (game.physics.arcade.distanceToXY(sprite, gotoX, sprite.y) > 8) {
+				game.physics.arcade.moveToXY(sprite, gotoX, sprite.y, 200);
 			}
+
+			if (game.physics.arcade.distanceToXY(sprite, sprite.x, gotoY) > 8) {
+				game.physics.arcade.moveToXY(sprite, sprite.x, gotoY, 200);
+			}
+
+		}
+		else {
+			sprite.body.velocity.set(0);
 		}
 
-		// Score
-		ctx.fillStyle = "rgb(250, 250, 250)";
-		ctx.font = "24px Helvetica";
-		ctx.textAlign = "left";
-		ctx.textBaseline = "top";
-		ctx.fillText("Players online: " + heroes.length, 32, 32);
-	};
+	}
 
-	// The main game loop
-	var main = function () {
-		var now = Date.now();
-		var delta = now - then;
+		function render () {
 
-		if (bgReady) {
-			ctx.drawImage(bgImage, 0, 0);
+			game.debug.inputInfo(32, 32);
+
 		}
-
-		drawBoard();
-
-		update(delta / 1000);
-		render(heroes);
-
-		then = now;
-		// Request to do this again ASAP
-		requestAnimationFrame(main);
-	};
-
-	// Cross-browser support for requestAnimationFrame
-	var w = window;
-	requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
-	canvas.addEventListener("click", getCursorPosition, false);
-	// Let's play this game!
-	var then = Date.now();
-	reset();
-	main();
-
-
-
-	
-	var myHero = {};
-
-	var nickname = prompt("Entre com seu nick : ", "");
-   	socket.emit('new player', {p: nickname});
-
-   	socket.on('myHero', function (hero) {
-   		myHero = hero;
-   	});
-
-   	socket.on('update players', function (_p) {
-   		heroes = Array();
-   		for (var i = 0; i < _p.length; i++) {
-   			heroes.push(createHero(_p[i]));
-   			render(heroes);
-   		}
-    });
-
-});
